3a5e486db18d4e70e437b404f3b7eedd8138869a
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-02-13 13:51:32 -0300

    Create Cliente controller.ts

---
+++ Cliente controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import * as clienteModel from '../models/clienteModel';
+
+// Função para obter os alunos
+export async function getCliente(req: Request, res: Response) {
+  try {
+    const cliente = await clienteModel.getClientes();
+    res.json(cliente);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar clientes' });
+  }
+}
+
+export async function criarCliente(req: Request, res: Response): Promise<any> {
+  const { nome, numero } = req.body;
+  if (!nome || !numero) {
+    return res.status(400).json({ message: 'Todos os campos devem ser preenchidos.' }); 
+  }
+
+  try {
+    const result = await clienteModel.criarCliente(nome, numero);
+    return res.status(201).json({ id: result.insertId }); 
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao criar cliente' }); 
+  }
+      }
