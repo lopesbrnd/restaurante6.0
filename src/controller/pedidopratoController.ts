48bdb1cb560cb35a67a75471a2cd0193397ea722
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-02-13 14:26:34 -0300

    Update pedidopratoController.ts

---
+++ src/controller/pedidopratoController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import * as garcomModel from '../models/edidoratoModel';
+
+// Função para obter os alunos
+export async function getPedidoprato(req: Request, res: Response) {
+    try {
+      const pedidoprato = await pedidopratoModel.getPedidoprato();
+      res.json(pedidoprato);
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao buscar os pedidos das mesas' });
+    }
+}
