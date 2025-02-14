995ac0cebee7b0d618506ac5a331330516a0d489
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-02-14 17:08:36 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config(); // Isso vai carregar as variáveis de ambiente do arquivo .env
 import clienteRoutes from './router/clienteRouter';
+import garcomRoutes from './router/garcomRouter';
+import mesaRoutes from './router/mesaRouter';
+import pedidopratoRoutes from './router/pedidopratoRouter';
+import pedidosRoutes from './router/pedidosRouter';
+import pratoRoutes from './router/pratoRouter';
 
 const app = express();
 
@@ -11,9 +16,10 @@ app.use(express.json()); // Para manipular requisições JSON
 
 // Configuração das rotas
 app.use('/api/clientes', clienteRoutes);
+app.use('/api/garcom', garcomRoutes);
+app.use('/api/mesa', mesaRoutes);
+app.use('/api/pedidoprato', pedidopratoRoutes);
+app.use('/api/pedidos', pedidosRoutes);
+app.use('/api/prato', pratoRoutes);
 
 console.log('Rotas carregadas corretamente!');
-
-app.listen(3000, () => {
-  console.log(`Servidor rodando em http://localhost:3000`);
-});
