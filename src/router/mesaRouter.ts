3a8de034b886c89ca7c03520b7fdb5cb6a97cdc5
gabii1210 <161453459+gabii1210@users.noreply.github.com>
2025-02-14 17:10:50 -0300

    Update mesaRouter.ts

--- src/router/mesaRouter.ts
+++ src/router/mesaRouter.ts
@@ -3,6 +3,7 @@ import * as mesaController from '../controller/mesaController';
 
 const router = express.Router();
 
-router.get('/', mesaController.getMesa);router.post
+router.get('/', mesaController.getMesa);
+router.post('/', mesaController.criarMesa);
 
 export default router;
